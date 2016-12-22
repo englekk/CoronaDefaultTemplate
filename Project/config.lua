5b15054bdde2a559a7915359ac982e5c20e9aa9a
KangminWon <englekk@naver.com>
2016-12-22 17:49:08 +0900

    changed config.lua

--- Project/config.lua
+++ Project/config.lua
@@ -8,41 +8,46 @@
 -- MIT License :: WonHada Library에 한정되며, 라이선스와 저작권 관련 명시만 지켜주면 되는 라이선스
 ----------------------------------------
 
--- 캐논 샘플을 참고함 :: https://github.com/coronalabs-samples/CoronaCannon
+-- !!! 아직 테스트가 충분하지 않지만 나쁘지 않은 것 같아서 적용합니다. 문제가 있다면 config.lua.bak 파일로 교체하시고 피드백(englekk@naver.com) 부탁드립니다.
 
--- 디자인 크기에 따라 fhd(FullHD, 1080x1920) 또는 hd(720x1280)로 변경하세요.
--- 디자인 파일이 많아서 용량을 줄이고 싶거나 퍼포먼스를 최적화해야 하는 경우라면 hd를 추천합니다.
-local designSize = "fhd"
+-- SmartPixel config.lua
+-- GitHub: https://github.com/Lerg/smartpixel-config-lua
 
 if not display then return end -- 데스크탑용 앱은 사용안함
 
--- 1080x1920 디자인 기준, 레터박스 스케일링용 계산법입니다.
--- x = 0, y = 0은 항상 화면의 좌상단을 가리킵니다.
--- 화면 우측 하단의 좌표를 구하려면, x = display.contentWidth, y = display.contentHeight를 사용하시면 됩니다.
 local w, h = display.pixelWidth, display.pixelHeight
 
--- 씬 디자인의 절반
--- 1080x1920의 경우 540x960이지만 레티나와 기본의 가로 비율을 갖게 하려면 640x960을 적용해야 함
--- 만약 모든 가로 비율을 동일하게(태블릿은 조금 더 큼) 하고 싶다면 아래 540을 640으로 수정하세요.
-local normalW, normalH = (w / h >= 0.6) and 640 or 540, 960 -- 씬 디자인이 1080x1920일 경우 (기본)
-if designSize == "hd" then normalW, normalH = (w / h >= 0.6) and 426.66 or 360, 640 end -- 씬 디자인이 720x1280일 경우
+local modes = {1, 1.5, 2, 3, 4, 6, 8} -- Scaling factors to try
+local contentW, contentH = 320, 480   -- Minimal size your content can fit in
 
-local scale = math.max(normalW / w, normalH / h)
-w, h = w * scale, h * scale
+-- Try each mode and find the best one
+local _w, _h, _m = w, h, 1
+for i = 1, #modes do
+    local m = modes[i]
+    local lw, lh = w / m, h / m
+    if lw < contentW or lh < contentH then
+        break
+    else
+        _w, _h, _m = lw, lh, m
+    end
+end
+-- If scaling is not pixel perfect (between 1 and 2) - use letterbox
+if _m < 2 then
+    local scale = math.max(contentW / w, contentH / h)
+    _w, _h = w * scale, h * scale
+end
 
 application = {
     content = {
-      width = w,
-        height = h,
+        width = _w,
+        height = _h,
         scale = 'letterbox',
         audioPlayFrequency = 44100, -- 11025, 22050, 44100: 높을수록 고음질
         fps = 60,
-        xAlign = "left",
-        yAlign = "top",
         imageSuffix = {
-        ['@2x'] = 1.1,
-              --['@4x'] = 2.1
-      }
+            ['@2x'] = 1.1,
+            ['@4x'] = 2.1,
+        }
     },
     --[[notification =
     {
